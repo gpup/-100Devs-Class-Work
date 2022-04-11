45da3ececef1c140931a7e8c8b28d0c6f441c27d
Seth Martin <jahgpup@gmail.com>
2022-04-11 11:27:50 -0600

    constructor project

--- objects-constructor-fun/js/main.js
+++ objects-constructor-fun/js/main.js
@@ -1,1 +1,15 @@
 //Create a constructor with 4 properties and 3 methods
+function MakeCar(carTransmission, carDrive, carEngineSize, carFuelType){
+    this.trans = carTransmission
+    this.drive = carDrive
+    this.output = carEngineSize
+    this.fuel = carFuelType
+    this.exhaustNote = function(){
+        alert('Deep Vroominess')
+    }
+    this.horn = function(){
+        alert('Toooooooot!')
+    }
+}
+
+let FordRanger = new MakeCar('manual', '4WD', '2.3L V6', 'Gas')
